5aa1922dc01321a713e8ac06854502159b9a12ee
rafeautie <rautie12@gmail.com>
2019-09-03 12:08:50 -0700

    Add visualizer boilerplate

---
+++ src/Visualizer/VisualizerStyle.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: calc(100vh - 75px);
+`;
---
+++ src/Visualizer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './VisualizerStyle';
+
+const Visualizer = () => {
+  return ( 
+    <Container>
+      
+    </Container>
+   );
+}
+
+export default Visualizer;
