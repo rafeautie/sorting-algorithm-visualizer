5b20f93274233c59f0e7278471ca2aba3db9de15
rafeautie <rautie12@gmail.com>
2019-09-05 13:55:54 -0700

    Implement P5 for maximum draw performance

--- src/Visualizer/index.js
+++ src/Visualizer/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Container } from './VisualizerStyle';
+import P5Wrapper from 'react-p5-wrapper';
+import sketch from './sketch';
 
-const Visualizer = () => {
-  return ( 
+const Visualizer = ({ lines }) => {
+  let s = sketch(lines);
+
+  return (
     <Container>
-      
+      <P5Wrapper sketch={s}/>
     </Container>
-   );
+  );
 }
 
 export default Visualizer;
