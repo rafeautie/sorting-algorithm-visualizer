f113451ef8aad86d5a34170bdcee2c1b33af11fa
rafeautie <rautie12@gmail.com>
2019-09-05 13:55:09 -0700

    Menu styling

--- src/Menu/MenuStyle.js
+++ src/Menu/MenuStyle.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: fixed;
+  bottom: 0;
   width: 100%;
   height: 75px;
   background: #414141;
+  z-index: 9999;
 `;
