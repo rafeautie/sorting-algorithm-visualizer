e199f1f49b8e47d6e3e32704fc04e5a6bb15a8d1
rafeautie <rautie12@gmail.com>
2019-09-05 13:54:51 -0700

    Implement number input and shuffle button

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Container } from './MenuStyle';
 
-const Menu = () => {
-  return ( 
+const Menu = ({ handleChange, handleShuffle, numOfLines }) => {
+  return (
     <Container>
-
+      <input
+        type="number"
+        min="5"
+        max="1000"
+        value={numOfLines}
+        onChange={({ target: { value } }) => handleChange(Number(value))}
+      ></input>
+      <button onClick={handleShuffle}></button>
     </Container>
-   );
+  );
 }
 
 export default Menu;
