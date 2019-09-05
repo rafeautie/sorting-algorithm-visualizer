9d839aacc66fb890bbab81709796a13e3ecb4526
rafeautie <rautie12@gmail.com>
2019-09-05 13:54:06 -0700

    Implement state using react hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
 import { Container } from './AppStyle';
-
 import Visualizer from './Visualizer';
 import Menu from './Menu'
 
+import LineCollection from './LineCollection';
+
 function App() {
+  const [numOfLines, setNumOfLines] = useState(5);
+  const [lines, setLines] = useState(new LineCollection(numOfLines));
+
+  console.log(lines);
+
+  const handleNumberChange = (num) => {
+    setNumOfLines(num);
+    setLines(new LineCollection(num));
+  }
+
+  const handleShuffle = () => {
+    setLines(LineCollection.shuffle(lines))
+  }
+
   return (
     <Container >
-      <Visualizer />
-      <Menu />
+      <Visualizer lines={lines} />
+      <Menu 
+        handleChange={handleNumberChange} 
+        handleShuffle={handleShuffle}
+        numOfLines={numOfLines}
+      />
     </Container>
   );
 }
