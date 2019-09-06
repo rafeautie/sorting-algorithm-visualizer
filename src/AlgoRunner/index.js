3eb978f615039b3116d015c7cd9716fe30b2539c
rafeautie <rautie12@gmail.com>
2019-09-06 10:38:43 -0700

    Implement AlgoRunner class

---
+++ src/AlgoRunner/index.js
@@ -0,0 +1,33 @@
+import * as ALGOS from '../sortingAlgorithms';
+
+export default class AlgoRunner {
+  constructor(lineCollection, algoToRun, setState) {
+    this.lineCollection = lineCollection;
+    this.algo = ALGOS[algoToRun];
+    this.setState = setState;
+    this.snapshots = [];
+    this.capture = this.capture.bind(this);
+  }
+
+  run() {
+    this.algo(this.lineCollection, this.capture);
+    this._startCycle();
+    return this;
+  }
+
+  capture(collectionSnapshot, [p1, p2]) {
+    //mark "p1" and "p2" in "collectionSnapshot" as swapping
+    this.snapshots.push(collectionSnapshot);
+  }
+
+  _startCycle() {
+    if (this.snapshots.length) {
+      this.setState(this.snapshots.shift());
+      setTimeout(this._continueCyle.bind(this), .1);
+    }
+  }
+
+  _continueCyle() {
+    this._startCycle();
+  }
+}
