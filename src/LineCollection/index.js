6f2c8452bc403ccb78b184c9f6782bb426d8f4a4
rafeautie <rautie12@gmail.com>
2019-09-05 20:52:07 -0700

    Improve line generation algorithm

--- src/LineCollection/index.js
+++ src/LineCollection/index.js
@@ -1,8 +1,9 @@
+import Line from './Line';
 import times from 'lodash/times';
 import shuffle from 'lodash/shuffle';
 
 class LineCollection extends Array {
-  constructor(input) {
+  constructor(input, randomized) {
     if(Array.isArray(input)) {
       super(...input);
     } else if (Number.isInteger(input)) {
@@ -11,28 +12,15 @@ class LineCollection extends Array {
     } else {
       super();
     }
+    
+    this.isRandom = randomized;
   }
 
   shuffle() {
     return new LineCollection(shuffle(this));
   }
-
-  _generateCollection(numberOfLines) {
-    let h = window.innerHeight - 75;
-    
-    times(numberOfLines, (i) => {
-      let color = this._genColor(i + 1);
-      let height = h / numberOfLines * (i + 1);
-      
-      this.push({ 
-        color, 
-        height, 
-        y: h - height,
-      });
-    });
-  }
-
-  _genColor(i) {
+  
+  calcColor(i) {
     let frequency = .060;
     let center = 128;
     let width = 127;
@@ -41,6 +29,20 @@ class LineCollection extends Array {
     var g = Math.floor(Math.sin(frequency * i + 2) * width + center);
     var b = Math.floor(Math.sin(frequency * i + 4) * width + center);
     return `rgb(${r}, ${g}, ${b})`;
+  }
+  
+  _generateCollection(numberOfLines) {
+    let canvasHeight = window.innerHeight - 75;
+    let width = window.innerWidth / numberOfLines;
+
+    times(numberOfLines, (i) => {
+      let height = canvasHeight / numberOfLines * (i + 1);
+      let color = this.calcColor(i + 1);
+      let x = width * i;
+      let y = canvasHeight - height;
+
+      this.push(new Line(x, y, width, height, color, numberOfLines, i))
+    });
   }
 }
 
