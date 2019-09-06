e74b267703dfbc12820835407abd2acd87177475
rafeautie <rautie12@gmail.com>
2019-09-06 10:38:09 -0700

    Implement run button

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import Visualizer from './Visualizer';
 import Menu from './Menu'
 
 import LineCollection from './LineCollection';
+import AlgoRunner from './AlgoRunner';
 
 function App() {
   const [numOfLines, setNumOfLines] = useState(5);
   const [lines, setLines] = useState(new LineCollection(numOfLines));
-
+  const [algoToRun, setAlgo] = useState('bubbleSort');
+  
   const handleNumberChange = ({ target: { value } }) => {
     let num = Number(value);
     setNumOfLines(num);
     setLines(new LineCollection(num));
   }
-
+  
   const handleShuffle = () => {
     setLines(lines.shuffle());
+  }
+  
+  const handleRun = () => {
+    let runner = new AlgoRunner(lines, algoToRun, setLines);
+    runner.run();
   }
 
   return (
@@ -25,6 +32,7 @@ function App() {
       <Menu
         handleChange={handleNumberChange}
         handleShuffle={handleShuffle}
+        handleRun={handleRun}
         numOfLines={numOfLines}
       />
     </Container>
