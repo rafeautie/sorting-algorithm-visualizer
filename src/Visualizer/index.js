4a26e1f0b179c3575e135415f80108cc385cd8a4
rafeautie <rautie12@gmail.com>
2019-09-05 20:54:21 -0700

    Provide num of lines to sketch props

--- src/Visualizer/index.js
+++ src/Visualizer/index.js
@@ -3,12 +3,12 @@ import { Container } from './VisualizerStyle';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketch';
 
-const Visualizer = ({ lines }) => {
+const Visualizer = ({ lines, numOfLines }) => {
   let s = sketch(lines);
 
   return (
     <Container>
-      <P5Wrapper sketch={s}/>
+      <P5Wrapper sketch={s} numOfLines={numOfLines}/>
     </Container>
   );
 }
