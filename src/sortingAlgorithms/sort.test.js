3baddfa878a0a707ab28a276aeb40ecdaba3cd79
rafeautie <rautie12@gmail.com>
2019-09-06 10:40:10 -0700

    Add test for sorting functionality

---
+++ src/sortingAlgorithms/sort.test.js
@@ -0,0 +1,35 @@
+import bubbleSort from './bubbleSort';
+
+describe('Bubble Sort', () => {
+
+
+  it('should sort an array', () => {
+    let shuffled = [
+      { height: 1 },
+      { height: 0 },
+      { height: 2 },
+      { height: 5 },
+      { height: 8 },
+      { height: 7 },
+      { height: 4 },
+      { height: 3 },
+      { height: 9 },
+      { height: 6 },
+    ]
+  
+    let expected = [
+      { height: 0 },
+      { height: 1 },
+      { height: 2 },
+      { height: 3 },
+      { height: 4 },
+      { height: 5 },
+      { height: 6 },
+      { height: 7 },
+      { height: 8 },
+      { height: 9 },
+    ]
+
+    expect(bubbleSort(shuffled)).toEqual(expected);
+  })
+})
