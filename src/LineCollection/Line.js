fcd91580235e65df68df7d30569ab728aee01abd
rafeautie <rautie12@gmail.com>
2019-09-05 20:51:45 -0700

    Add Line class

---
+++ src/LineCollection/Line.js
@@ -0,0 +1,19 @@
+export default class Line {
+  constructor(x, y, w, h, c, n, i) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.color = c;
+    this.numberOfLines = n;
+    this._previousCanvasHeight = window.innerHeight - 75;
+    this.i = i;
+  }
+
+  recalculate(i) {
+    this.height = (window.innerHeight - 75) / this.numberOfLines * (this.i + 1);
+    this.width = window.innerWidth / this.numberOfLines;
+    this.x = this.width * i;
+    this.y = (window.innerHeight - 75) - this.height;
+  }
+}
