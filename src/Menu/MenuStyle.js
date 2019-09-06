8cb8b8d3773c31c11dea4b919af927edca629415
rafeautie <rautie12@gmail.com>
2019-09-05 20:53:16 -0700

    Change background color to white

--- src/Menu/MenuStyle.js
+++ src/Menu/MenuStyle.js
@@ -1,10 +1,35 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  align-items: center;
   position: fixed;
   bottom: 0;
   width: 100%;
   height: 75px;
-  background: #414141;
+  background: white;
   z-index: 9999;
+  padding: 0 25px;
+
+  .ig {
+    width: 155px;
+  }
+`;
+
+export const Input = styled.input`
+  margin-left: 25px;
+  border-radius: 5px;
+  padding: 12px;
+  outline: none;
+  border: none;
+  font-size: 1rem;
+`;
+
+export const Button = styled.button`
+  margin-left: 25px;
+  border-radius: 5px;
+  border: none;
+  padding: 12px;
+  outline: none;
+  font-size: 15px;
 `;
