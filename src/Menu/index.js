e88ded4a5a059a93facef9b5cdef00356d3e3864
rafeautie <rautie12@gmail.com>
2019-09-05 20:54:04 -0700

    Implement bootstrap

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Container } from './MenuStyle';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 const Menu = ({ handleChange, handleShuffle, numOfLines }) => {
   return (
     <Container>
-      <input
-        type="number"
-        min="5"
-        max="1000"
-        value={numOfLines}
-        onChange={({ target: { value } }) => handleChange(Number(value))}
-      ></input>
-      <button onClick={handleShuffle}></button>
+      <InputGroup className='ig'>
+        <FormControl
+          type="number"
+          value={numOfLines}
+          onChange={handleChange}
+        />
+        <InputGroup.Append>
+          <Button
+            variant="dark"
+            onClick={handleShuffle}
+          >Shuffle</Button>
+        </InputGroup.Append>
+      </InputGroup>
     </Container>
   );
 }
