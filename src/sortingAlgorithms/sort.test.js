8a6ec69ba803123cb7252bc0ecf2755f2b417b88
rafeautie <rautie12@gmail.com>
2019-09-07 07:30:33 -0700

    Add test for merge sort

--- src/sortingAlgorithms/sort.test.js
+++ src/sortingAlgorithms/sort.test.js
@@ -1,8 +1,9 @@
-import bubbleSort from './bubbleSort';
+import {
+  bubbleSort,
+  // mergeSort,
+} from './index';
 
 describe('Bubble Sort', () => {
-
-
   it('should sort an array', () => {
     let shuffled = [
       { height: 1 },
@@ -16,7 +17,7 @@ describe('Bubble Sort', () => {
       { height: 9 },
       { height: 6 },
     ]
-  
+
     let expected = [
       { height: 0 },
       { height: 1 },
@@ -31,5 +32,37 @@ describe('Bubble Sort', () => {
     ]
 
     expect(bubbleSort(shuffled)).toEqual(expected);
+  })
+})
+
+describe('Merge Sort', () => {
+  it('should sort an array', () => {
+    let shuffled = [
+      { height: 1 },
+      { height: 0 },
+      { height: 2 },
+      { height: 5 },
+      { height: 8 },
+      { height: 7 },
+      { height: 4 },
+      { height: 3 },
+      { height: 9 },
+      { height: 6 },
+    ]
+
+    let expected = [
+      { height: 0 },
+      { height: 1 },
+      { height: 2 },
+      { height: 3 },
+      { height: 4 },
+      { height: 5 },
+      { height: 6 },
+      { height: 7 },
+      { height: 8 },
+      { height: 9 },
+    ]
+
+    expect(mergeSort(shuffled)).toEqual(expected);
   })
 })
