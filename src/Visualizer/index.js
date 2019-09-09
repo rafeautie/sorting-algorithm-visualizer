4fc84dea87844896c263594045d9ec1364d1f1e5
rafeautie <rautie12@gmail.com>
2019-09-09 13:01:45 -0700

    Remove unused prop

--- src/Visualizer/index.js
+++ src/Visualizer/index.js
@@ -3,12 +3,13 @@ import { Container } from './VisualizerStyle';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketch';
 
-const Visualizer = ({ lines, numOfLines }) => {
+const Visualizer = ({ lines }) => {
+  console.log(lines);
   let s = sketch(lines);
 
   return (
     <Container>
-      <P5Wrapper sketch={s} numOfLines={numOfLines}/>
+      <P5Wrapper sketch={s} />
     </Container>
   );
 }
