ae70514c8b51b66bfecdc01e055805630662f3cf
rafeautie <rautie12@gmail.com>
2019-09-09 19:25:26 -0700

    Add selectAlgorithm reducer to main reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
-import updateLineCollection from './updateLineCollection';
+import lineCollection from './updateLineCollection';
+import selectAlgorithm from './selectAlgorithm';
 
 export default combineReducers({
-  lineCollection: updateLineCollection,
+  lineCollection,
+  selectAlgorithm,
 })
