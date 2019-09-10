e713116156069db43a723d54a2887789d469b244
rafeautie <rautie12@gmail.com>
2019-09-09 21:51:56 -0700

    Add isRunning main reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import lineCollection from './lineCollection';
 import algoToRun from './algoToRun';
+import isRunning from './isRunning';
 
 export default combineReducers({
   lineCollection,
   algoToRun,
+  isRunning,
 })
