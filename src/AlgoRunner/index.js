427bb958bf9426b2dc6fcd2ccec0393831522359
rafeautie <rautie12@gmail.com>
2019-09-10 01:53:15 -0700

    Implement speed and store subscription

--- src/AlgoRunner/index.js
+++ src/AlgoRunner/index.js
@@ -1,14 +1,21 @@
 import store from '../redux/store';
-import { updateLineCollectionAction } from '../redux/actionCreators';
+import { 
+  updateLineCollectionAction,
+} from '../redux/actionCreators';
 import * as ALGOS from '../sortingAlgorithms';
 import delay from 'lodash/delay';
 
 export default class AlgoRunner {
   constructor(algoToRun, done) {
     this.lineCollection = store.getState().lineCollection;
+    this.speed = store.getState().speed
     this.algo = ALGOS[algoToRun](this.lineCollection, 0, this.lineCollection.length - 1);
     this.snapshot = this.algo.next();
     this.done = done;
+    this.unsubscribe = store.subscribe(() => {
+      let s = store.getState().speed
+      this.speed = 1000 - (s / 10 * 100);
+    })
   }
 
   run() {
@@ -19,9 +26,10 @@ export default class AlgoRunner {
     if (!this.snapshot.done) {
       store.dispatch(updateLineCollectionAction(this.snapshot.value));
       this.snapshot = this.algo.next();
-      delay(this._continueCyle.bind(this), 0);
+      delay(this._continueCyle.bind(this), this.speed);
     } else {
       this.done();
+      this.unsubscribe();
     }
   }
 
