b64a47ceebab71b95c66e7d53738c3853fed6683
rafeautie <rautie12@gmail.com>
2019-09-09 23:24:40 -0700

    Export updateLineCountAction

--- src/redux/actionCreators/index.js
+++ src/redux/actionCreators/index.js
@@ -2,10 +2,12 @@ import updateLineCollectionAction from './updateLineCollectionAction';
 import shuffleLineCollectionAction from './shuffleLineCollectionAction';
 import algoToRunAction from './algoToRunAction';
 import toggleRunningAction from './toggleRunningAction';
+import updateLineCountAction from './updateLineCountAction';
 
 export {
   algoToRunAction,
   toggleRunningAction,
+  updateLineCountAction,
   updateLineCollectionAction,
   shuffleLineCollectionAction,
 };
