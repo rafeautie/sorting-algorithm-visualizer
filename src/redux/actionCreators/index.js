e34b13a859597cde2e5ecef775357054f8889dbd
rafeautie <rautie12@gmail.com>
2019-09-09 21:50:51 -0700

    Export toggleRunning action

--- src/redux/actionCreators/index.js
+++ src/redux/actionCreators/index.js
@@ -1,9 +1,11 @@
 import updateLineCollectionAction from './updateLineCollectionAction';
 import shuffleLineCollectionAction from './shuffleLineCollectionAction';
 import algoToRunAction from './algoToRunAction';
+import toggleRunningAction from './toggleRunningAction';
 
 export {
+  algoToRunAction,
+  toggleRunningAction,
   updateLineCollectionAction,
   shuffleLineCollectionAction,
-  algoToRunAction,
 };
