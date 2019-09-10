bc2a0462793865468a7ef9b751797921c5248b24
rafeautie <rautie12@gmail.com>
2019-09-10 01:54:10 -0700

    Export speed action

--- src/redux/actionCreators/index.js
+++ src/redux/actionCreators/index.js
@@ -3,9 +3,11 @@ import shuffleLineCollectionAction from './shuffleLineCollectionAction';
 import algoToRunAction from './algoToRunAction';
 import toggleRunningAction from './toggleRunningAction';
 import updateLineCountAction from './updateLineCountAction';
+import updateSpeedAction from './updateSpeedAction';
 
 export {
   algoToRunAction,
+  updateSpeedAction,
   toggleRunningAction,
   updateLineCountAction,
   updateLineCollectionAction,
