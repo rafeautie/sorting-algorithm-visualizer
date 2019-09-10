6f08a24e6dccd0ebb0e677e5b8e3f152437ecd42
rafeautie <rautie12@gmail.com>
2019-09-10 12:56:15 -0700

    Implement mark on comparison

--- src/sortingAlgorithms/insertionSort.js
+++ src/sortingAlgorithms/insertionSort.js
@@ -4,11 +4,25 @@ export default function* (arr) {
   for (let i = 1; i < arr.length; i++) {
     let idx = i;
     while (idx > 0 && arr[idx].height < arr[idx - 1].height) {
-      let lineBeforeCurrent = arr[idx - 1];
-      arr[idx - 1] = arr[idx];
-      arr[idx] = lineBeforeCurrent;
+      let p1 = arr[idx - 1];
+      let p2 = arr[idx];
+
+      p1.mark(true);
+      p2.mark(true);
+      
       yield new LineCollection(arr);
+
+      arr[idx - 1] = p2;
+      arr[idx] = p1;
+      
+      yield new LineCollection(arr);
+      
+      p1.mark(false);
+      p2.mark(false);
+
       idx--
     }
   }
+  //Finall yield used to clear last marked lines
+  yield new LineCollection(arr);
 }
