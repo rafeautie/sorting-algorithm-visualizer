0902411b217ccfba0907a3f46ea5fdb58e18f770
rafeautie <rautie12@gmail.com>
2019-09-10 01:55:01 -0700

    Add speed to main reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,10 +3,12 @@ import lineCollection from './lineCollection';
 import algoToRun from './algoToRun';
 import isRunning from './isRunning';
 import lineCount from './lineCount';
+import speed from './updateSpeed';
 
 export default combineReducers({
   lineCollection,
   algoToRun,
   isRunning,
   lineCount,
+  speed,
 })
