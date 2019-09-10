082e904d606d44f03e83ccf9c9bdac722394f4c2
rafeautie <rautie12@gmail.com>
2019-09-09 20:51:54 -0700

    Remove unused callbacks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Visualizer } from './redux/containers';
-import { Container } from './AppStyle';
 import Options from './Options'
 
-import LineCollection from './LineCollection';
-import AlgoRunner from './AlgoRunner';
-
-function App() {
-  const [numOfLines, setNumOfLines] = useState(50);
-  const [lines, setLines] = useState(new LineCollection(numOfLines).shuffle());
-  const [algoToRun, setAlgo] = useState('mergeSort');
-  const [drawerIsOpen, toggleDrawer] = useState(true);
-
-  const handleLineCountChange = (_, num) => {
-    setNumOfLines(num);
-    setLines(new LineCollection(num));
-  }
-
-  const handleShuffle = () => {
-    setLines(lines.shuffle());
-  }
-
-  const handleRun = () => {
-    let runner = new AlgoRunner(lines, algoToRun, setLines);
-    runner.run();
-  }
-
-  const handleAlgoChange = (e) => {
-    setAlgo(e.target.value);
-  }
-
+const App = () => {
   return (
-    <Container >
+    <>
       <Options />
-      <Visualizer lines={lines} numOfLines={numOfLines} />
-    </Container>
+      <Visualizer />
+    </>
   );
 }
 
