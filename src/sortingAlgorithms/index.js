89b50db1b75456ab7a3b422686c69d8f05f9ff9e
rafeautie <rautie12@gmail.com>
2019-09-14 08:02:20 -0700

    Implement selection sort

--- src/sortingAlgorithms/index.js
+++ src/sortingAlgorithms/index.js
@@ -2,8 +2,10 @@ import bubbleSort from './bubbleSort';
 import mergeSort from './mergeSort';
 import insertionSort from './insertionSort';
 import quickSort from './quickSort';
+import selectionSort from './selectionSort';
 
 export {
+  selectionSort,
   insertionSort,
   bubbleSort,
   mergeSort,
---
+++ src/sortingAlgorithms/selectionSort.js
@@ -0,0 +1,17 @@
+import LineCollection from '../LineCollection';
+
+export default function* (a) {
+  let len = a.length;
+  debugger;
+
+  for (let i = 0; i < len; i++) {
+    let min = i;
+
+    for (let j = i + 1; j < len; j++) {
+      if (a[j].height < a[min].height) min = j;
+    }
+
+    [a[i], a[min]] = [a[min], a[i]];
+    yield new LineCollection(a);
+  }
+}
