0f4f4cda21e6e2b420441a0c45c4807c5a170731
rafeautie <rautie12@gmail.com>
2019-09-14 16:49:44 -0700

    Export stats reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,11 +4,15 @@ import algoToRun from './algoToRun';
 import isRunning from './isRunning';
 import lineCount from './lineCount';
 import speed from './updateSpeed';
+import swapCount from './incrementSwaps';
+import comparisonCount from './incrementComparisons';
 
 export default combineReducers({
   lineCollection,
+  comparisonCount,
   algoToRun,
   isRunning,
   lineCount,
   speed,
+  swapCount,
 })
