a67075ddc4d97f05ac525e4182fa63d68b39c112
rafeautie <rautie12@gmail.com>
2019-09-14 16:49:04 -0700

    Export Stats container

--- src/redux/containers/index.js
+++ src/redux/containers/index.js
@@ -1,9 +1,11 @@
 import Visualizer from './VisualizerContainer';
 import AlgoSelect from './AlgoSelectContainer';
 import SliderCard from './SliderCardContainer';
+import Stats from './StatsContainer';
 
 export {
   Visualizer,
   AlgoSelect,
-  SliderCard
+  SliderCard,
+  Stats
 };
