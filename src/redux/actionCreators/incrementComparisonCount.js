5c65e6a5383f370f7b11f177b4ea27fa8ff07a7c
rafeautie <rautie12@gmail.com>
2019-09-14 16:48:10 -0700

    Add action reducer for comparison count

---
+++ src/redux/actionCreators/incrementComparisonCount.js
@@ -0,0 +1,5 @@
+export default function incrementComparisonCount() {
+  return {
+    type: 'INCREMENT_COMPARISON_COUNT'
+  }
+}
---
+++ src/redux/actionCreators/incrementSwapCount.js
@@ -0,0 +1,5 @@
+export default function incrementSwapCount() {
+  return {
+    type: 'INCREMENT_SWAP_COUNT'
+  }
+}
