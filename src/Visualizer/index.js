12284fb257b0fe0942f836d31a023e6995d295dd
rafeautie <rautie12@gmail.com>
2019-10-02 17:38:41 -0700

    Implement Worker

--- src/Visualizer/index.js
+++ src/Visualizer/index.js
@@ -6,17 +6,39 @@ import sketch from './sketch';
 const Visualizer = ({ lines }) => {
   // let s = sketch(lines);
   const canvas = useRef(null);
+  const offscreen = useRef(null);
+  const worker = useRef(null);
 
   useEffect(() => {
-    const offscreen = canvas.current.transferControlToOffscreen();
-    const worker = new Worker('ww.js');
+    //FIX RESOLUTION OF CANVAS
+    if (!worker.current) worker.current = new Worker('ww.js');
 
-    worker.postMessage({ canvas: offscreen, lines }, [offscreen]);
+    //Hard coded dpi.
+    let dpi = 2;
+
+    //GET height and width of canvas.
+    let style_height = +getComputedStyle(canvas.current).getPropertyValue("height").slice(0, -2);
+    let style_width = +getComputedStyle(canvas.current).getPropertyValue("width").slice(0, -2);
+    
+    //Set height and width of canvas.
+    canvas.current.setAttribute('height', style_height * dpi);
+    canvas.current.setAttribute('width', style_width * dpi);
+
+    //Transfer canvas to offscreen.
+    offscreen.current = canvas.current.transferControlToOffscreen();
+
+    //Transfer offscreen canvas to Worker.
+    worker.current.postMessage({ canvas: offscreen.current, lines }, [offscreen.current]);
 
     return () => {
-      worker.terminate();
+      //Terminate Worker on will unmount.
+      worker.current.terminate();
     }
-  }, [lines]);
+  }, [])
+
+  useEffect(() => {
+    worker.current.postMessage({ canvas: null, lines });
+  });
 
   return (
     <Container>
